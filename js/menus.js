8114a17db9364142d3cc3407482c00bfe4311519
Dono <dono@dfn.online>
2019-11-06 17:23:26 -0800

    test

--- js/menus.js
+++ js/menus.js
@@ -8,12 +8,19 @@ function openMenu(menu) {
   let elements = document.querySelectorAll('div:not(#' + menu +')');
   elements.forEach((index) => {
     index.addEventListener("click", closeMenu)
+    if (menu == "login") {
+      index.style.opacity = ".2";
+    }
+
   })
   function closeMenu() {
     event.stopPropagation();
     document.getElementById(menu).style.display = "none";
     elements.forEach((index) => {
-      index.removeEventListener("click", closeMenu)
+      index.removeEventListener("click", closeMenu);
+      if (menu == "login") {
+        index.style.opacity = "1";
+      }
     })
   }
 };
